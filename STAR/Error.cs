95fcff44a93cb8cfb1fb119f6e41e02a98e01929
dmconnolly <danny@flau.co.uk>
2016-09-13 12:34:38 +0100

    Improved storage of packet data, added accessors and fixed a of couple issues

--- STAR/Error.cs
+++ STAR/Error.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace STAR {
     class Error : Message {
-        private String errorString;
+        private string errorString;
 
         // Takes date string in the form dd-MM-yyyy HH:mm:ss.FFFF
         // and a string containing the error message
-        public Error(String dateString, String errorString) : base(dateString) {
+        public Error(string dateString, string errorString) : base(dateString) {
             this.errorString = errorString;
         }
 
         // Accessor for error string
-        public override String ToString() {
+        public override string ToString() {
             return errorString;
         }
     }
--- STAR/Message.cs
+++ STAR/Message.cs
@@ -7,34 +7,36 @@ using System.Threading.Tasks;
 
 namespace STAR {
     class Message {
-        private const String timeFormat = "dd-MM-yyyy HH:mm:ss.ffff";
+        private const String timeFormat = "dd-MM-yyyy HH:mm:ss.fff";
         private DateTime timestamp;
-
-        // Takes date string in the form dd-MM-yyyy HH:mm:ss.FFFF
-        public Message(String dateString) {
-            // Take date as string and store as DateTime
-            this.timestamp = parseDateString(dateString);
-        }
 
         // Gets the timestamp as millionths of a second
         // useful for ordering packets and errors on the UI
-        public long getTime() {
-            return this.timestamp.Ticks;
+        public long Time {
+            get {
+                return this.timestamp.Ticks;
+            }
         }
 
         // Gets the timestamp as a formatted string
-        public String getTimeString() {
-            return String.Format("{0:" + timeFormat + "}", timestamp);
+        public String getTimeString {
+            get {
+                return String.Format("{0:" + timeFormat + "}", timestamp);
+            }
+        }
+
+        // Takes date string in the form dd-MM-yyyy HH:mm:ss.fff
+        public Message(String dateString) {
+            // Take date as string and store as DateTime
+            this.timestamp = parseDateString(dateString);
         }
 
         // Parse the date string and return a DateTime
         private DateTime parseDateString(String dateString) {
             DateTime timestamp = new DateTime();
 
-            CultureInfo provider = CultureInfo.InvariantCulture;
-
             try {
-                timestamp = DateTime.ParseExact(dateString, timeFormat, provider);
+                timestamp = DateTime.ParseExact(dateString, timeFormat, CultureInfo.InvariantCulture);
             } catch(FormatException) {
                 Console.WriteLine("{0} is not in the correct format.", dateString);
             } 
