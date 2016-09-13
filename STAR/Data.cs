2b91da0cdfb2f42670dabf8506ad5c1306e6a43e
Auctioneer <ljgdavie@hotmail.com>
2016-09-13 12:06:24 +0100

    Can read in data to string. Added temp button to form.

--- STAR/Data.cs
+++ STAR/Data.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,19 +16,42 @@ namespace STAR
 
         //Start and end timestamps
         DateTime startTimeStamp;
-
         DateTime endTimeStamp;
+
+        //All the data in string format
+        string fileText;
+
+        //Hard-coded file location - change later
+        private string fileLoc =
+            @"C:\Users\Lewis\Documents\My Documents\University\Fourth Year\Industrial Project\team_project_example_files\test1_link1.rec";
+
+        //Reads the entirety of the data file into a string
+        public void ReadFile()
+        {
+            FileStream fs = new FileStream(@fileLoc, FileMode.Open, FileAccess.Read);
+            
+            using (var streamReader = new StreamReader(fs, Encoding.UTF8))
+            {
+                fileText = streamReader.ReadToEnd();
+            }
+
+            Console.WriteLine(fileText);
+        }
 
     }
 
-    //Read in first line
-    //Read in port
-    //ERROR packets to go packet, error messages go to message->error
+    //Create list of messages 
+    //Read in first line and store in startTimeStamp
+    //Read in port and store in port
+    //ERROR packets to go packet, error messages go to message->errormessage
+    //Note: Packets are of type message
 
     //Ignore blank lines
     //Store temp variables - timestamp
-    //If it's a packet 'P' - get cargo on next line, then EOP or EEP 
-
+    //If it's a packet 'P' - get cargo on next line, then EOP or EEP or None
+    //If it's E, that's an ErrorMessage. Error message on next line (e.g. Disconnect)
+    //Add all the data to the message list, with the temp variables passed into the constructor
+    //If the line was disconnect, skip a line then store endTimeStamp
 
 
 
--- STAR/Form1.Designer.cs
+++ STAR/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnReadData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReadData
+            // 
+            this.btnReadData.Location = new System.Drawing.Point(13, 13);
+            this.btnReadData.Name = "btnReadData";
+            this.btnReadData.Size = new System.Drawing.Size(75, 23);
+            this.btnReadData.TabIndex = 0;
+            this.btnReadData.Text = "Read data";
+            this.btnReadData.UseVisualStyleBackColor = true;
+            this.btnReadData.Click += new System.EventHandler(this.btnReadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnReadData);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnReadData;
     }
 }
 
