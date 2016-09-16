76d2428f1065bff2d6b5efd38feb56b482dfa61b
john1884 <john.bothwell1884@gmail.com>
2016-09-16 14:28:43 +0100

    oops. Wrong folder

--- STAR/View/Main.xaml.cs
+++ STAR/View/Main.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace STAR.ViewModel
+namespace STAR.View
 {
     /// <summary>
     /// Interaction logic for Main.xaml
