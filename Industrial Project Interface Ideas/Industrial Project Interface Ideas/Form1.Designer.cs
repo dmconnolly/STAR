88ffc213fbd9254e3c2cd2cf43ff500f071d2ca6
rimills <r.i.mills@dundee.ac.uk>
2016-09-16 13:54:20 +0100

    Entered some initial code for filtering packets - Will be integrated with main system when available.

--- Industrial Project Interface Ideas/Industrial Project Interface Ideas/Form1.Designer.cs
+++ Industrial Project Interface Ideas/Industrial Project Interface Ideas/Form1.Designer.cs
@@ -74,6 +74,7 @@
             this.UploadButton.TabIndex = 2;
             this.UploadButton.Text = "Upload Traffic File";
             this.UploadButton.UseVisualStyleBackColor = false;
+            this.UploadButton.Click += new System.EventHandler(this.UploadButton_Click);
             // 
             // label1
             // 
@@ -182,7 +183,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1054, 517);
+            this.ClientSize = new System.Drawing.Size(1055, 531);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
--- Industrial Project Interface Ideas/Industrial Project Interface Ideas/Form1.cs
+++ Industrial Project Interface Ideas/Industrial Project Interface Ideas/Form1.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 
 namespace Industrial_Project_Interface_Ideas
 {
     public partial class TrafficDataForm : Form
     {
+
         public TrafficDataForm()
         {
             InitializeComponent();
@@ -44,6 +47,117 @@ namespace Industrial_Project_Interface_Ideas
             ErrorForm newForm = new ErrorForm();
             newForm.Show();
             Hide();
+        }
+
+        private void UploadButton_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line = null;
+            DateTime startTime = new DateTime();
+            string[] packet = new string[1234567890];
+            DateTime endTime = new DateTime();
+            string[] errorLocation = new string[1234567890];
+            bool[] packetInFilter = new bool[1234567890];
+            string[] filteredPackets = new string[1234567890];
+            bool[][] errorType = new bool[3][];
+            bool errorPresent = false;
+            int pointer = 0;
+
+
+            for (pointer = 0; pointer < errorLocation.Length; pointer++)
+            {
+                for (int innerPointer = 0; innerPointer < 3; innerPointer++)
+                {
+                    errorType[innerPointer][pointer] = false;
+                }
+            }
+
+            // Read the file and display it line by line.
+            StreamReader file = new StreamReader("c:\\test.txt");
+            line = file.ReadLine();
+            do
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    counter++;
+                    if (line == "E")
+                    {
+                        errorType[0][counter] = true;
+                        errorLocation[counter] = line;
+                        errorPresent = true;
+                    } else if (line == "EEP")
+                    {
+                        errorType[1][counter] = true;
+                        errorLocation[counter] = line;
+                        errorPresent = true;
+                    } else if (line == "None")
+                    {
+                        errorType[2][counter] = true;
+                        errorLocation[counter] = line;
+                        errorPresent = true;
+                    }
+                }
+            } while (errorPresent == false);
+
+            file.Close();
+
+
+            // Suspend the screen.
+            Console.ReadLine();
+
+            //Searches for every error of a given type
+            bool[] errorSelection = new bool[3];
+            errorSelection[0] = true;
+            errorSelection[1] = true;
+            errorSelection[2] = true;
+
+            if (errorSelection[0] == true)
+            {
+                for (pointer = 0; pointer < errorLocation.Length; pointer++)
+                {
+                    if (errorType[0][pointer] == true)
+                    {
+                        packetInFilter[pointer] = true;
+                    }
+                }
+            }
+            if (errorSelection[1] == true)
+            {
+                for (pointer = 0; pointer < errorLocation.Length; pointer++)
+                {
+                    if (errorType[1][pointer] == true)
+                    {
+                        packetInFilter[pointer] = true;
+                    }
+                }
+            }
+            if (errorSelection[2] == true)
+            {
+                for (pointer = 0; pointer < errorLocation.Length; pointer++)
+                {
+                    if (errorType[2][pointer] == true)
+                    {
+                        packetInFilter[pointer] = true;
+                    }
+                }
+            }
+            counter = 0;
+            for (pointer = 0; pointer < packetInFilter.Length; pointer++)
+            {
+                if (packetInFilter[pointer] == true)
+                {
+                    filteredPackets[counter] = packet[pointer];
+                    counter++;
+                }
+            }
+            for (pointer = 0; pointer < filteredPackets.Length; pointer++)
+            {
+                while (filteredPackets[pointer] != null)
+                {
+                    Console.Write(filteredPackets[pointer]);
+                }
+            }
         }
     }
 }
