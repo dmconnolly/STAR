087dd8cfe4a73e9ccc68be6d5cfa42c68495308f
rimills <r.i.mills@dundee.ac.uk>
2016-09-19 17:07:35 +0100

    Editing of UI to aid with statistic display, and addition of statistic quality array, which will change the colour of the text box for the displays

--- STAR/ViewModel/Statistics.cs
+++ STAR/ViewModel/Statistics.cs
@@ -17,6 +17,7 @@ namespace STAR {
         public double totalPacketsPerSecond;
         public double totalErrorsPerSecond;
         public double totalBytesPerSecond;
+        public string[] statisticQuality; //Used to change the colour of the statistics settings
 
         public Statistics() {
             Clear();
@@ -48,6 +49,104 @@ namespace STAR {
             totalPacketsPerSecond = packetCount / measurementTimeSeconds;
             totalErrorsPerSecond = errorMessageCount / measurementTimeSeconds;
             totalBytesPerSecond = totalByteCount / measurementTimeSeconds;
+
+                if (packetCount > 100) //Getting statistic quality of packet count
+                {
+                    statisticQuality[0] = "Good";
+                } else if (packetCount > 50)
+                {
+                    statisticQuality[0] = "Normal";
+                }
+                else
+                {
+                    statisticQuality[0] = "Bad";
+                }
+            if (validPacketCount / packetCount > 0.75) //Getting statistic quality of valid packet count
+            {
+                statisticQuality[1] = "Good";
+            }
+            else if (validPacketCount / packetCount > 0.5)
+            {
+                statisticQuality[1] = "Normal";
+            }
+            else
+            {
+                statisticQuality[1] = "Bad";
+            }
+            if (invalidPacketCount / packetCount < 0.25) //Getting statistic quality of invalid packet count
+            {
+                statisticQuality[2] = "Good";
+            }
+            else if (invalidPacketCount / packetCount < 0.5)
+            {
+                statisticQuality[2] = "Normal";
+            }
+            else
+            {
+                statisticQuality[2] = "Bad";
+            }
+            if (errorMessageCount < 3) //Getting statistic quality of error message count
+            {
+                statisticQuality[3] = "Good";
+            }
+            else if (errorMessageCount < 7)
+            {
+                statisticQuality[3] = "Normal";
+            }
+            else
+            {
+                statisticQuality[3] = "Bad";
+            }
+            if (totalByteCount > 1000) //Getting statistic quality of byte count
+            {
+                statisticQuality[4] = "Good";
+            }
+            else if (totalByteCount > 500)
+            {
+                statisticQuality[4] = "Normal";
+            }
+            else
+            {
+                statisticQuality[4] = "Bad";
+            }
+            if (totalPacketsPerSecond > 20) //Getting statistic quality of packets per second
+            {
+                statisticQuality[5] = "Good";
+            }
+            else if (totalPacketsPerSecond > 10)
+            {
+                statisticQuality[5] = "Normal";
+            }
+            else
+            {
+                statisticQuality[5] = "Bad";
+            }
+            if (totalErrorsPerSecond < 1) //Getting statistic quality of errors per second
+            {
+                statisticQuality[6] = "Good";
+            }
+            else if (totalErrorsPerSecond < 2)
+            {
+                statisticQuality[6] = "Normal";
+            }
+            else
+            {
+                statisticQuality[6] = "Bad";
+            }
+            if (totalBytesPerSecond > 200) //Getting statistic quality of characters per second
+            {
+                statisticQuality[7] = "Good";
+            }
+            else if (totalBytesPerSecond > 100)
+            {
+                statisticQuality[7] = "Normal";
+            }
+            else
+            {
+                statisticQuality[7] = "Bad";
+            }
+
+
         }
 
         public void Clear() {
