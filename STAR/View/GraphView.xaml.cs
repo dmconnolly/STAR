0ce3cf098ef45c0454606d78fa0c89ca92503459
rimills <r.i.mills@dundee.ac.uk>
2016-09-22 15:10:27 +0100

    Attempt to pass Statistics to GraphView

--- STAR/View/GraphView.xaml.cs
+++ STAR/View/GraphView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Controls;
 using System.Windows.Controls.DataVisualization.Charting;
 using STAR.ViewModel;
 
@@ -14,63 +15,8 @@ namespace STAR.View
         {
             InitializeComponent();
 
-            Statistics stats = new Statistics();
-
-            //int[] errorPacketsPerSecond;
-            //int[] dataCharactersPerSecond;
-            //int[] packetsPerSecond;
-            //DateTime[] secondsForPacket;
-            //bool matchedSecond;
-
-
-            //Pseudo based on tutorial added iterator and dynamic sizing instead of constant.
-
-            //value = new listofvalues <x value, y value>();
-
-            /* Method for generating the statistics for generating a graph
-             * foreach (PacketView packetView in Main.PacketCollectionView) {
-             *      if (graphInput == "ErrorRate") {
-             *           int pointer = 0;
-             *           matchedSecond = false;
-             *           if (packetView.PacketTypeString.Equals("Error") {
-             *              do {
-             *                   if (packetView.m_timeInSeconds = secondForPacket[pointer]) {
-             *                      errorPacketsPerSecond[pointer]++;
-             *                      matchedSecond = true;
-             *                   }
-             *              } while ((secondForPacket[pointer] != null) && (matchedSecond != true))
-             *              if (matchedSecond == false) {
-             *                  secondForPacket[pointer] = packetView.m_timeInSeconds;
-             *              }
-             *          }
-             *      }else if (graphInput == "DataRate") {
-             *          int pointer = 0;
-             *           matchedSecond = false;
-             *              do {
-             *                   if (packetView.m_timeInSeconds = secondForPacket[pointer]) {
-             *                      dataCharactersPerSecond[pointer] = dataCharactersPerSecond[pointer] + packetView.DataBytes.Length();
-             *                      matchedSecond = true;
-             *                   }
-             *              } while ((secondForPacket[pointer] != null) && (matchedSecond != true))
-             *              if (matchedSecond == false) {
-             *                  secondForPacket[pointer] = packetView.m_timeInSeconds;
-             *              }
-             *      }else if (graphInput == "PacketRate") {
-             *          int pointer = 0;
-             *           matchedSecond = false;
-             *              do {
-             *                   if (packetView.m_timeInSeconds = secondForPacket[pointer]) {
-             *                      packetsPerSecond[pointer]++;
-             *                      matchedSecond = true;
-             *                   }
-             *              } while ((secondForPacket[pointer] != null) && (matchedSecond != true))
-             *              if (matchedSecond == false) {
-             *                  secondForPacket[pointer] = packetView.m_timeInSeconds;
-             *              }
-             *     }
-             * for (i = 0; i < list.size(); i++)
-             * value.Add(i, elem.at);
-             */
+            Capture statisticCapture = new Capture();
+            Statistics statistics = statisticCapture.Stats; 
 
             //Note: Pull statistics from Capture Class, not from Statistics
             Chart graph = this.FindName("XyChart") as Chart;
