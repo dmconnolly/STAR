09ab16e5c4c6381fdee7f36324abbfc26d9825de
rimills <r.i.mills@dundee.ac.uk>
2016-09-26 13:14:05 +0100

    Fixing errors caused by the merge.

--- STAR/ViewModel/PacketView.cs
+++ STAR/ViewModel/PacketView.cs
@@ -14,78 +14,40 @@ namespace STAR.ViewModel
     {
         private const string timeFormatUI = "HH:mm:ss.fff";
 
-<<<<<<< HEAD
-        public long TimeTicks { get; private set; }
-        public string TimeString { get; private set; }
-        public byte EntryPort { get; private set; }
-        public byte ExitPort { get; private set; }
-        public Type PacketType { get; private set; }
-        public bool DataPacket { get; private set; }
-        public string PacketTypeString { get; private set; }
+        public bool Valid                         { get; private set; }
+        public bool CRCError                      { get; private set; }
+        public bool SequenceIdError               { get; private set; }
+        public bool DuplicatePacketError          { get; private set; }
+        public bool DataPacket                    { get; private set; }
+        public Type PacketType                    { get; private set; }
+        public long TimeTicks                     { get; private set; }
 
-        public string Message          { get; private set; }
-        public string EndCode          { get; private set; }
-        public bool   Valid            { get; private set; }
-        public bool   CRCError         { get; private set; }
-        public bool   SequenceIdError  { get; private set; }
-        public bool   DuplicatePacketError { get; private set; }
-        public byte[] Cargo { get; private set; }
-
-        public ushort ProtocolId           { get; private set; }
-        public byte   DestinationKey       { get; private set; }
-        public uint   SequenceId           { get; private set; }
-        public byte[] SourcePathAddress    { get; private set; }
-        public byte   SourceLogicalAddress { get; private set; }
-        public ushort TransactionId        { get; private set; }
-        public byte   ExtendedWriteAddress { get; private set; }
-        public uint   WriteAddress         { get; private set; }
-        public uint   ReadAddress          { get; private set; }
-        public uint   DataLength           { get; private set; }
-        public byte   HeaderCRC            { get; private set; }
-        public byte[] DataBytes            { get; private set; }
-        public byte   DataCRC              { get; private set; }
-        public byte   Status               { get; private set; }
-        public byte   ReplyCRC             { get; private set; }
-        public byte   DestinationLogicalAddress { get; private set; }
+        public string TimeString                  { get; private set; }
+        public string EntryPort                   { get; private set; }
+        public string ExitPort                    { get; private set; }
+        public string PacketTypeString            { get; private set; }
+        public string Message                     { get; private set; }
+        public string EndCode                     { get; private set; }
+        public string Cargo                       { get; private set; }
+        public string ProtocolId                  { get; private set; }
+        public string   DestinationKey            { get; private set; }
+        public string   SequenceId                { get; private set; }
+        public string SourcePathAddress           { get; private set; }
+        public string   SourceLogicalAddress      { get; private set; }
+        public string TransactionId               { get; private set; }
+        public string   ExtendedWriteAddress      { get; private set; }
+        public string   WriteAddress              { get; private set; }
+        public string   ReadAddress               { get; private set; }
+        public string   DataLength                { get; private set; }
+        public string   HeaderCRC                 { get; private set; }
+        public string DataBytes                   { get; private set; }
+        public string   DataCRC                   { get; private set; }
+        public string   Status                    { get; private set; }
+        public string   ReplyCRC                  { get; private set; }
+        public string   DestinationPathAddress    { get; private set; }
+        public string   DestinationLogicalAddress { get; private set; }
 
         
-
-        public PacketView() {
-=======
-        public bool Valid { get; private set; }
-        public bool CRCError { get; private set; }
-        public bool SequenceIdError { get; private set; }
-        public bool DuplicatePacketError { get; private set; }
-        public bool DataPacket { get; private set; }
-        public Type PacketType { get; private set; }
-        public long TimeTicks { get; private set; }
->>>>>>> 04dc3e7a9fa920a1d8a787577a84d9fb305b9b01
-
-        public string TimeString { get; private set; }
-        public string EntryPort { get; private set; }
-        public string ExitPort { get; private set; }
-        public string PacketTypeString { get; private set; }
-        public string Message { get; private set; }
-        public string EndCode { get; private set; }
-        public string Cargo { get; private set; }
-        public string ProtocolId { get; private set; }
-        public string DestinationKey { get; private set; }
-        public string SequenceId { get; private set; }
-        public string SourcePathAddress { get; private set; }
-        public string SourceLogicalAddress { get; private set; }
-        public string TransactionId { get; private set; }
-        public string ExtendedWriteAddress { get; private set; }
-        public string WriteAddress { get; private set; }
-        public string ExtendedReadAddress { get; private set; }
-        public string ReadAddress { get; private set; }
-        public string DataLength { get; private set; }
-        public string HeaderCRC { get; private set; }
-        public string DataBytes { get; private set; }
-        public string DataCRC { get; private set; }
-        public string Status { get; private set; }
-        public string ReplyCRC { get; private set; }
-        public string DestinationPathAddress { get; private set; }
-        public string DestinationLogicalAddress { get; private set; }
 
         // Constructor for PacketView
         // Takes a Packet of any time as a parameter
@@ -140,69 +102,9 @@ namespace STAR.ViewModel
             {
                 NonRmapPacket pkt = packet as NonRmapPacket;
                 PacketTypeString = "Non RMAP";
-<<<<<<< HEAD
-            }
-            else if (PacketType == typeof(WriteCommandPacket))
-            {
-                SequenceId = pkt.SequenceNumber;
-                Cargo = pkt.Cargo;
             } else if(PacketType == typeof(WriteCommandPacket)) {
                 WriteCommandPacket pkt = packet as WriteCommandPacket;
                 PacketTypeString = "Write command";
-                DestinationKey = pkt.DestinationKey;
-                SourcePathAddress = pkt.SourcePathAddress;
-                SourceLogicalAddress = pkt.SourceLogicalAddress;
-                TransactionId = pkt.TransactionId;
-                ExtendedWriteAddress = pkt.ExtendedWriteAddress;
-                WriteAddress = pkt.WriteAddress;
-                DataLength = pkt.DataLength;
-                HeaderCRC = pkt.HeaderCRC;
-                DataBytes = pkt.DataBytes;
-                DataCRC = pkt.DataCRC;
-            }
-            else if (PacketType == typeof(WriteResponsePacket))
-            {
-                WriteResponsePacket pkt = packet as WriteResponsePacket;
-                PacketTypeString = "Write response";
-                Status = pkt.Status;
-                DestinationLogicalAddress = pkt.DestinationLogicalAddress;
-                TransactionId = pkt.TransactionId;
-                ReplyCRC = pkt.ReplyCRC;
-            }
-            else if (PacketType == typeof(ReadCommandPacket))
-            {
-                ReadCommandPacket pkt = packet as ReadCommandPacket;
-                PacketTypeString = "Read command";
-                DestinationKey = pkt.DestinationKey;
-                SourcePathAddress = pkt.SourcePathAddress;
-                SourceLogicalAddress = pkt.SourceLogicalAddress;
-                TransactionId = pkt.TransactionId;
-                ExtendedWriteAddress = pkt.ExtendedWriteAddress;
-                ReadAddress = pkt.ReadAddress;
-                DataLength = pkt.DataLength;
-                HeaderCRC = pkt.HeaderCRC;
-            }
-            else
-            {
-                ReadResponsePacket pkt = packet as ReadResponsePacket;
-                PacketTypeString = "Read response";
-                Status = pkt.Status;
-                DestinationLogicalAddress = pkt.DestinationLogicalAddress;
-                TransactionId = pkt.TransactionId;
-                DataLength = pkt.DataLength;
-                HeaderCRC = pkt.HeaderCRC;
-                DataBytes = pkt.DataBytes;
-                DataCRC = pkt.DataCRC;
-=======
-                DestinationPathAddress = byteToString(pkt.PathAddress);
-                DestinationLogicalAddress = byteToString(pkt.LogicalAddress);
-                SequenceId = byteToString(pkt.SequenceNumber);
-                Cargo = byteToString(pkt.Cargo);
-            } else if(PacketType == typeof(WriteCommandPacket)) {
-                WriteCommandPacket pkt = packet as WriteCommandPacket;
-                PacketTypeString = "Write command";
-                DestinationPathAddress = byteToString(pkt.PathAddress);
-                DestinationLogicalAddress = byteToString(pkt.LogicalAddress);
                 DestinationKey = byteToString(pkt.DestinationKey);
                 SourcePathAddress = byteToString(pkt.SourcePathAddress);
                 SourceLogicalAddress = byteToString(pkt.SourceLogicalAddress);
@@ -213,33 +115,33 @@ namespace STAR.ViewModel
                 HeaderCRC = byteToString(pkt.HeaderCRC);
                 DataBytes = byteToString(pkt.DataBytes);
                 DataCRC = byteToString(pkt.DataCRC);
-            } else if(PacketType == typeof(WriteReplyPacket)) {
+            }
+            else if (PacketType == typeof(WriteReplyPacket))
+            {
                 WriteReplyPacket pkt = packet as WriteReplyPacket;
-                PacketTypeString = "Write reply";
-                SourcePathAddress = byteToString(pkt.PathAddress);
-                SourceLogicalAddress = byteToString(pkt.LogicalAddress);
+                PacketTypeString = "Write response";
                 Status = byteToString(pkt.Status);
                 DestinationLogicalAddress = byteToString(pkt.DestinationLogicalAddress);
                 TransactionId = byteToString(pkt.TransactionId);
                 ReplyCRC = byteToString(pkt.ReplyCRC);
-            } else if(PacketType == typeof(ReadCommandPacket)) {
+            }
+            else if (PacketType == typeof(ReadCommandPacket))
+            {
                 ReadCommandPacket pkt = packet as ReadCommandPacket;
                 PacketTypeString = "Read command";
-                DestinationPathAddress = byteToString(pkt.PathAddress);
-                DestinationLogicalAddress = byteToString(pkt.LogicalAddress);
                 DestinationKey = byteToString(pkt.DestinationKey);
                 SourcePathAddress = byteToString(pkt.SourcePathAddress);
                 SourceLogicalAddress = byteToString(pkt.SourceLogicalAddress);
                 TransactionId = byteToString(pkt.TransactionId);
-                ExtendedReadAddress = byteToString(pkt.ExtendedReadAddress);
+                ExtendedWriteAddress = byteToString(pkt.ExtendedReadAddress);
                 ReadAddress = byteToString(pkt.ReadAddress);
                 DataLength = byteToString(pkt.DataLength);
                 HeaderCRC = byteToString(pkt.HeaderCRC);
-            } else {
+            }
+            else
+            {
                 ReadReplyPacket pkt = packet as ReadReplyPacket;
-                PacketTypeString = "Read reply";
-                SourcePathAddress = byteToString(pkt.PathAddress);
-                SourceLogicalAddress = byteToString(pkt.LogicalAddress);
+                PacketTypeString = "Read response";
                 Status = byteToString(pkt.Status);
                 DestinationLogicalAddress = byteToString(pkt.DestinationLogicalAddress);
                 TransactionId = byteToString(pkt.TransactionId);
@@ -258,7 +160,6 @@ namespace STAR.ViewModel
             }
             if(tmp.Length % 2 != 0) {
                 tmp = "0" + tmp;
->>>>>>> 04dc3e7a9fa920a1d8a787577a84d9fb305b9b01
             }
             return tmp;
         }
